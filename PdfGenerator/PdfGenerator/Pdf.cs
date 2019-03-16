6a77e3814d3742b85dac5678a4864e3f323c9215
Karan Bhamra <karan.bhamra@gmail.com>
2019-03-16 14:39:24 -0700

    Refactored pdf logic into Pdf.cs class, can save to byte array or to a file with specified path; TODO: html template logic

---
+++ PdfGenerator/PdfGenerator/Pdf.cs
@@ -0,0 +1,83 @@
+using System;
+using System.IO;
+using DinkToPdf;
+using DinkToPdf.Contracts;
+
+namespace PdfGenerator
+{
+    public class Pdf
+    {
+        private string Title { get; set; }
+        private GlobalSettings globalSettings;
+        private ObjectSettings objectSettings;
+
+        private HtmlToPdfDocument pdfDocument;
+
+        private IConverter Converter { get; set; }
+
+        private string htmlData;
+        
+        public Pdf(string title, string data)
+        {
+            Title = title;  
+            Converter = new SynchronizedConverter(new PdfTools());
+            htmlData = data;
+            setDocumentSettings();
+
+        }
+
+        private void setDocumentSettings()
+        {
+            
+            globalSettings = new GlobalSettings()
+            {
+                ColorMode = ColorMode.Color,
+                Orientation = Orientation.Portrait,
+                PaperSize = PaperKind.A4,
+                Margins = new MarginSettings { Top = 10 },
+                DocumentTitle = Title,
+            };
+
+            objectSettings = new ObjectSettings()
+            {
+                PagesCount = true,
+                HtmlContent = htmlData,//TemplateGenerator.GetHTMLString(),    // HTML String Goes here for the data
+                // PDF stylesheet is saved into PdfGenerator/assets/styles.css
+                WebSettings = { DefaultEncoding = "utf-8", UserStyleSheet =  Path.Combine(Directory.GetCurrentDirectory(), "assets", "styles.css") },
+                HeaderSettings = { FontName = "Arial", FontSize = 9, Right = "Page [page] of [toPage]", Line = true },
+                FooterSettings = { FontName = "Arial", FontSize = 9, Line = true, Center = "CSUN Student Report" }
+
+            };
+
+            pdfDocument = new HtmlToPdfDocument()
+            {
+                GlobalSettings = globalSettings,
+                Objects = {objectSettings}
+            };
+
+        }
+
+
+        public byte[] SaveToByteArray()
+        {
+            return Converter.Convert(pdfDocument);
+        }
+
+        public bool SaveToFile(string path)
+        {
+            byte[] fileBytes = SaveToByteArray();
+
+            try
+            {
+                File.WriteAllBytes(path, fileBytes);
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return false;
+            }
+            
+        }
+    }
+}
--- PdfGenerator/PdfGenerator/Program.cs
+++ PdfGenerator/PdfGenerator/Program.cs
@@ -1,9 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Reflection;
-using DinkToPdf;
-using DinkToPdf.Contracts;
 
 namespace PdfGenerator
 {
@@ -12,42 +7,20 @@ namespace PdfGenerator
     {
         static void Main(string[] args)
         {
-            string path = Path.Combine(Directory.GetCurrentDirectory(), "output", "test.pdf");
+            
+            Pdf mydoc = new Pdf("CSUN Student Report", TemplateGenerator.GetHTMLString());
 
-//            Console.WriteLine(path);
-            Console.WriteLine(path);
-            
-            IConverter converter = new SynchronizedConverter(new PdfTools());
-            
-            var globalSettings = new GlobalSettings
-            {
-                ColorMode = ColorMode.Color,
-                Orientation = Orientation.Portrait,
-                PaperSize = PaperKind.A4,
-                Margins = new MarginSettings { Top = 10 },
-                DocumentTitle = "PDF Report",
-                Out = path//$"{Directory.GetCurrentDirectory() + "\test.pdf"}"   //@"D:\PDFCreator\Employee_Report.pdf"
-            };
- 
-            var objectSettings = new ObjectSettings
-            {
-                PagesCount = true,
-                HtmlContent = TemplateGenerator.GetHTMLString(),
-                WebSettings = { DefaultEncoding = "utf-8", UserStyleSheet =  Path.Combine(Directory.GetCurrentDirectory(), "assets", "styles.css") },
-                HeaderSettings = { FontName = "Arial", FontSize = 9, Right = "Page [page] of [toPage]", Line = true },
-                FooterSettings = { FontName = "Arial", FontSize = 9, Line = true, Center = "Report Footer" }
-            };
- 
-            var pdf = new HtmlToPdfDocument()
-            {
-                GlobalSettings = globalSettings,
-                Objects = { objectSettings }
-            };
+            var result = mydoc.SaveToFile("testfile.pdf");
 
-            converter.Convert(pdf);
-            
-            Console.WriteLine("Converted pdf successfully.");
-            //*/
+            if (result)
+            {
+                Console.WriteLine("Converted pdf successfully.");
+            }
+            else
+            {
+                Console.WriteLine("Failed to save pdf");
+            }
+
         }
     }
 }
